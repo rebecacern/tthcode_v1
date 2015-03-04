e4ccf5209ed3cc8782bd154d87f50ebc311234fe
Rebeca <rebeca@cern.ch>
2015-03-04 16:02:10 +0100

    latest

--- minimal.C
+++ minimal.C
@@ -10,6 +10,7 @@ void minimal(int nsel=0, int mode=0, bool silent=0){
   TFile *fin = new TFile("../tuples/synch_full_second.root");
   TTree *tree = (TTree*)fin->Get("OSTwoLepAna/summaryTree");
 
+  // Tree branches
   Double_t        mcwgt;
   Double_t	  wgt;
   Double_t	  wallTimePerEvent;
@@ -44,7 +45,8 @@ void minimal(int nsel=0, int mode=0, bool silent=0){
   tree->SetBranchAddress("preselected_electrons", &preselected_electrons, &b_preselected_electrons);
   tree->SetBranchAddress("preselected_muons", &preselected_muons, &b_preselected_muons);
   tree->SetBranchAddress("preselected_leptons", &preselected_leptons, &b_preselected_leptons);
-
+  ////
+  
   if (mode != 0 && mode !=1 && mode !=2) mode = 0;
   if (!silent){
     cout << "[Info:]" ;
@@ -59,6 +61,7 @@ void minimal(int nsel=0, int mode=0, bool silent=0){
     std::cout << "results root file named " << newRootFile << std::endl;
   }
 
+  // Histos
   char title[300];
   sprintf(title,"cuts");
   TH1F* histo = new TH1F( title, " ", 20, 0, 20 );
@@ -71,38 +74,55 @@ void minimal(int nsel=0, int mode=0, bool silent=0){
   // loop over events 
   for(int iEvent = 0; iEvent < tree->GetEntries(); iEvent++){
     Long64_t tentry = tree->LoadTree(iEvent);
-    
+    //Point to the proper entry
     b_preselected_electrons->GetEntry(tentry);
     b_preselected_muons->GetEntry(tentry);
     b_preselected_leptons->GetEntry(tentry);
     b_higgs_decay->GetEntry(tentry);
      
+    //do stuff 
     histo->Fill(0., weight);
+    
     if (!higgs_decay) continue;
     histo->Fill(1., weight);
+    
     if (preselected_leptons->size() < 2) continue;
     histo->Fill(2., weight);
  
     ttH::Lepton lepton1 = preselected_leptons->at(0);
     ttH::Lepton lepton2 = preselected_leptons->at(1);
-    bool goodemu = false;
-    if (mode == 0 && preselected_muons->size() !=0 && preselected_electrons->size() != 0){
-      if (abs(lepton1.pdgID) !=  abs(lepton2.pdgID))  goodemu = true; 
-    }
-    if (mode == 0 && !goodemu) continue;
+    
+    if (mode == 0 && abs(lepton1.pdgID) ==  abs(lepton2.pdgID)) continue;
     if (mode == 1 && (abs(lepton1.pdgID) != 13 ||  abs(lepton2.pdgID) != 13)) continue;
     if (mode == 2 && (abs(lepton1.pdgID) != 11 ||  abs(lepton2.pdgID) != 11)) continue;
     histo->Fill(3., weight);
 
     if (lepton1.charge!=lepton2.charge) continue;
- 
     histo->Fill(4., weight);
 	      
     if (lepton1.tlv().Pt() < 20) continue;
     if (lepton2.tlv().Pt() < 20)  continue;
    
     histo->Fill(5., weight);
- 
+    // Work in progress here
+    //  Cut-based selection
+    // relative isolation (R=0.3,rho*EA) < 0.1
+    // sip3D < 4 
+    //electrons
+    //Phys14 POG MVA ID (Tight WP) defined on slide5 here
+    //lost hits == 0
+    //passConversionVeto
+    //in 2lss final state only
+    // isGsfCtfScPixChargeConsistent
+    bool passcut_1 = false;
+    if (lepton1.relIso < 0.1 && lepton1.sip3D < 4) {
+      if (abs(lepton1.pdgID) == 11){
+         ttH::Electron electron = preselected_electrons->at(0);
+	 cout << electron.mvaID << endl;
+      }
+    }
+  
+
    
   }
   
